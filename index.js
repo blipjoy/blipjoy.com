8912bac6ef1f364c6ef5de32ee0fcb177444bcb4
Jay Oster <jay@kodewerx.org>
2022-02-07 02:48:29 -0800

    Add "lightbox" for the screenshots

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 import { BlipJoyAnimation } from './js/anim.js';
 
 function main() {
+  // Start the logo animation
   new BlipJoyAnimation();
+
+  // Fake lightbox for the screenshots
+  const links = document.querySelectorAll('p.screenshot > a');
+  for (const link of links) {
+    link.addEventListener('click', (event) => {
+      if (event instanceof PointerEvent) {
+        lightbox_click(link, event);
+      }
+    });
+  }
+}
+
+/**
+ * @arg {Element} anchor
+ * @arg {PointerEvent} event
+ */
+function lightbox_click(anchor, event) {
+  if (event.button !== 0) {
+    return;
+  }
+
+  const img = anchor.querySelector('img');
+  if (img === null) {
+    return;
+  }
+
+  img.classList.toggle('full');
+  event.preventDefault();
 }
 
 if (document.readyState === 'loading') {
